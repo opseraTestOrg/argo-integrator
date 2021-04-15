6714e0e19614f0510d2fbcef2907bbc6d10772a4
Vimal Villavan <vimal@opsera.io>
2021-04-15 11:42:16 -0700

    rolling update

--- scripts/build/dev-docker.sh
+++ scripts/build/dev-docker.sh
@@ -12,7 +12,7 @@ date_tag=`date +%-d-%m-%Y-%T | sed 's/:/-/g'`
 docker tag opsera-argo-integrator:kube 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:kube-${date_tag}
 docker tag opsera-argo-integrator:kube 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:latest
 
-docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:kube-dev-${date_tag}
+docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:kube-${date_tag}
 docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:latest
 
 
