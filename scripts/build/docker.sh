4259bbdc66c22daf98cf7ecf8093e32730df2adf
Vimal Villavan <vimal@opsera.io>
2021-04-15 15:47:26 -0700

    rolling update, clean up build jobs

---
+++ scripts/build/docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xe
+docker build -t opsera-argo-integrator:latest  ../../
+docker run --rm \
+        440953937617.dkr.ecr.us-east-2.amazonaws.com/kubectl \
+        aws ecr get-login-password \
+        --region us-east-2 \
+        | docker login --username AWS \
+        --password-stdin 440953937617.dkr.ecr.us-east-2.amazonaws.com
+
+date_tag=`date +%-d-%m-%Y-%T | sed 's/:/-/g'`
+docker tag opsera-argo-integrator:latest 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:kube-${date_tag}
+docker tag opsera-argo-integrator:latest 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:latest
+
+docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:kube-${date_tag}
+docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-argo-integrator:latest
+
+
