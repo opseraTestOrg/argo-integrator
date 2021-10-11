16d08dfe6f77728014cecf7bc9409c628c522541
Srinath <srinath@opsera.io>
2021-10-11 12:55:18 +0530

    OPL-1285 Eureka Service discovery removal

--- src/main/java/com/opsera/integrator/argo/ArgoIntegratorApplication.java
+++ src/main/java/com/opsera/integrator/argo/ArgoIntegratorApplication.java
@@ -2,10 +2,8 @@ package com.opsera.integrator.argo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ArgoIntegratorApplication {
 
     /**
